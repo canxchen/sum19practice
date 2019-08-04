aa90078b7643c8d4e32d56097f00078aee556036
canxchen <35909292+canxchen@users.noreply.github.com>
2019-08-04 11:18:02 -0400

    Update pacificAtlanticWater.java
    dfs: columns, then rows. at each dfa, check if the indices that ur checking are inbounds, and if its been visited before, and if its value is < the index that you came from (RMB THAT YOU CAN ONLY GO TO SPACES THAT ARE <= YOU so if you<that_space its wrong)
    mark it as seen
    do dfs in all directions 
    the syntax for for loop is:
    for (type name:array) {
      do something with type

--- 417. pacificAtlWaterFlow/pacificAtlanticWater.java
+++ 417. pacificAtlWaterFlow/pacificAtlanticWater.java
@@ -1,21 +1,28 @@
 import java.util.*;
 public class pacificAtlanticWater {
-    public List<List<Integer>> pacificAtlantic(int[][] matrix) {
-        //[down][across]
-        List<List<Integer>> ans = new ArrayList<>();
-        if(matrix==null||matrix.length==0) return ans;
-        int m = matrix.length; //down
-        int n = matrix[0].length; //across
-        boolean[][] canReachPac = new boolean[matrix.length][matrix[0].length];
+    int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+    public List<int[]> pacificAtlantic(int[][] matrix) { //[down][across]
+        List<int[]> ans = new ArrayList<>();
+        if(matrix == null || matrix.length == 0 || matrix[0].length==0) return ans;
+
+        int m = matrix[0].length; //across
+        int n = matrix.length; //down
+        boolean[][] atl = new boolean[m][n]; boolean[][] pac = new boolean[m][n];
+        for (int i = 0; i < n; i++) { //go down
+            dfs(matrix,pac,Integer.MIN_VALUE,i,0); //first column
+            dfs(matrix,atl,Integer.MIN_VALUE,i,m-1); //last column column
+        }
+        for (int i = 0; i < m; i++) { //do across
+            dfs(matrix,pac,Integer.MIN_VALUE,0,i); //first row
+            dfs(matrix,atl,Integer.MIN_VALUE,n-1,i); //last row
+        }
+
+        /*boolean[][] canReachPac = new boolean[matrix.length][matrix[0].length];
         boolean[][] canReachAtl = new boolean[matrix.length][matrix[0].length];
         for (int i = 0; i < n; i++) //go across: left down, pacific
-            canReachPac[0][i] = true;
-        for (int i = 0; i < m; i++) //go down
-            canReachPac[i][0] = true;
-        for (int i = 0; i < n; i++) //go across: right down, atlantic
-            canReachAtl[m - 1][i] = true; //last row
-        for (int i = 0; i < m; i++) //go down, last column
-            canReachAtl[i][n - 1] = true;
+            canReachAtl[m - 1][i] = canReachPac[0][i] = true;
+        for (int i = 0; i < m; i++)
+            canReachAtl[i][n - 1] = canReachPac[i][0] = true;
 
         for (int i = 1; i < m; i++) { //down
             for (int j = 1; j < n; j++) { //left to right
@@ -33,10 +40,18 @@ public class pacificAtlanticWater {
             for (int j = 0; j < m; j++) {
                 if (canReachPac[i][j] && canReachAtl[i][j]) {
                     ArrayList<Integer> add = new ArrayList<>();
-                    add.add(i);add.add(j);ans.add(add);
                 }
             }
-        }
+        }*/
+        ans.add(new int[]{1,1});
         return ans;
+    }
+    public void dfs(int[][]matrix,boolean[][]visited,int height, int i, int j) {
+        int n = matrix.length, m = matrix[0].length;
+        if (i<0 || i>=n || j<0 || j>=m || visited[i][j] || matrix[i][j] < height) return;
+        visited[i][j]=true;
+        for (int[]d:directions) {
+            dfs(matrix, visited,matrix[i][j], i+d[0],j+d[1]);
+        }
     }
 }
